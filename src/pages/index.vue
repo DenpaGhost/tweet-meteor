abd74dca012ad4d87ee46e3c030a20ce00caa5a4
DenpaGhost <az.kinosaki@gmail.com>
2021-02-17 18:14:54 +0900

    initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style lang="scss">
+.container {
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+}
+</style>
